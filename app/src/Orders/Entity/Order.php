db705aa4ddc10ce8cdd2154249f09b207b48b04c
Dan <denncath@gmail.com>
2025-11-03 05:33:08 +0200

    Added warehousing reservation tests

--- app/src/Orders/Entity/Order.php
+++ app/src/Orders/Entity/Order.php
@@ -8,7 +8,6 @@ use App\Orders\Enum\OrderLineStatus;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'orders')]
