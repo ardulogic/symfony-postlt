063a77d2fe453176ce7b4369e2ad90ce71233701
Dan <denncath@gmail.com>
2025-11-03 08:53:46 +0200

    Added remaining status "Shipped Partial"

--- app/src/Orders/Enum/OrderLineStatus.php
+++ app/src/Orders/Enum/OrderLineStatus.php
@@ -8,5 +8,7 @@ enum OrderLineStatus: string
     case RESERVED           = 'RESERVED';
     case RESERVED_PARTIAL   = 'RESERVED_PARTIAL';
     case SHIPPED            = 'SHIPPED';
+    case SHIPPED_PARTIAL    = 'SHIPPED_PARTIAL';
     case CANCELED           = 'CANCELED';
+    case OUT_OF_STOCK       = 'OUT_OF_STOCK';
 }
